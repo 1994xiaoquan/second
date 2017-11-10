6b92f14d4ad7daf1834005fe3a106a40aaad9203
小全 <995680996@qq.com>
2017-11-10 22:23:26 +0800

    第五天上传

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/manager/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/manager/service/ItemService.java
@@ -1,7 +1,10 @@
 package cn.e3mall.manager.service;
 
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
+import cn.e3mall.utils.E3mallResult;
 import cn.e3mall.utils.PageBeanResult;
+import cn.e3mall.utils.PictureResult;
 
 public interface ItemService {
 	/**
@@ -13,4 +16,13 @@ public interface ItemService {
 	 * 分页查询
 	 */
 	public PageBeanResult  getListByPage(Integer page,Integer rows);
+	
+	/**
+	 * 添加商品
+	 * 
+	 * 参数:Tbitem, IbItemDesc
+	 * 返回值:E3mallResult
+	 */
+	public E3mallResult  insertItem(TbItem tbitem,TbItemDesc itemdesc);
+	
 }
--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/manager/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/manager/service/impl/ItemServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.e3mall.manager.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +10,21 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
 import cn.e3mall.manager.service.ItemService;
+import cn.e3mall.mapper.TbItemDescMapper;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
 import cn.e3mall.pojo.TbItemExample;
+import cn.e3mall.utils.E3mallResult;
+import cn.e3mall.utils.IDUtils;
 import cn.e3mall.utils.PageBeanResult;
+import cn.e3mall.utils.PictureResult;
 @Service
 public class ItemServiceImpl implements ItemService{
 	@Autowired
 	private TbItemMapper itemMapper;
+	@Autowired
+	private TbItemDescMapper  itemDescMapper;
 	@Override
 	public TbItem findItemById(Long itemId) {
 		return itemMapper.selectByPrimaryKey(itemId);
@@ -38,6 +46,32 @@ public class ItemServiceImpl implements ItemService{
 		result.setTotal(pageInfo.getTotal());
 		result.setRows(list);
 		return result;
+	}
+	/**
+	 * 
+	 */
+	public E3mallResult insertItem(TbItem item, TbItemDesc desc) {
+		//补全属性
+		//id
+		//
+		//设置Id
+		//使用工具类生成ID
+		long itemId = IDUtils.genItemId();
+		item.setId(itemId);
+		//补全时间类型
+		Date date = new Date();
+		item.setCreated(date);
+		item.setUpdated(date);
+		//补全类型
+		item.setStatus((byte)1);
+		//保存商品
+		itemMapper.insert(item);
+		//补全商品描述信息属性
+		desc.setItemId(itemId);
+		desc.setCreated(date);
+		desc.setUpdated(date);
+		itemDescMapper.insert(desc);
+		return E3mallResult.ok();
 	}
 
 }
